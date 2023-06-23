fa89046ce5d658b80f8105a784ba71c270697689
raposu <raposudesoto@gmail.com>
2023-06-23 16:11:45 +0200

    adding layouts

--- layouts/aux_functions.py
+++ layouts/aux_functions.py
@@ -1,37 +1,150 @@
 import pandas as pd
 import numpy as np
+import plotly.graph_objs as go
 import plotly.express as px
 from layouts.constants import month_list, degree_sign
 from data.postgresql.data_fetch import data_fetch
-from data.postgresql.queries import query_records, query_records_month, query_tmed, query_year
+from data.postgresql.queries import query_records, query_records_month, query_year_prec, query_year_anom, query_tmed, query_prec_month,query_prec_year
 
-def extract_data(column, estacion, tab, month):
+def extract_data(column, estacion, tab, month=None):
+    order = tab[-3:]
 
-        order = tab[-3:]
+    if month:
+        df = pd.DataFrame(data_fetch(query_records_month(column, estacion, order, month)), columns=['fecha', column])
+    else:
+        df = pd.DataFrame(data_fetch(query_records(column, estacion, order)), columns=['fecha', column])
 
-        if month:
-            df = pd.DataFrame(data_fetch(query_records_month(column, estacion, order, month)), columns=['fecha',column])
+    result = []
+    for i in range(5):
+        fecha = df.iloc[i].fecha
+        result.append('{}, {} {}'.format(fecha.year, fecha.day, month_list[fecha.month - 1]))
+        temperature = df.iloc[i][column]
+        if column == 'prec':
+            result.append('{:.1f} mm'.format(temperature))
         else:
-            df = pd.DataFrame(data_fetch(query_records(column, estacion, order)), columns=['fecha',column])
+            result.append('{:.1f}{}'.format(temperature, degree_sign + 'C'))
+    return tuple(result)
+#######################################################################################################
+#######################################################################################################
+def extract_prec_month(estacion):
+    query = query_prec_month(estacion)
+    results = data_fetch(query)
 
-        result = []
-        for i in range(5):
-            fecha = df.iloc[i].fecha
-            result.append('{}, {} {}'.format(fecha.year, fecha.day, month_list[fecha.month-1]))
-            temperature = df.iloc[i][column]
-            if column == 'prec':
-                result.append('{:.1f}'.format(temperature) + ' mm')
-            else:
-                result.append('{:.1f}'.format(temperature) + degree_sign + 'C')
-        return tuple(result)
+    dates = ['{} {}'.format(month_list[result[0] - 1], result[1]) for result in results]
+    precipitations = ['{:.1f} mm'.format(result[2]) for result in results]
+    return (
+        dates[0], precipitations[0],
+        dates[1], precipitations[1],
+        dates[2], precipitations[2],
+        dates[3], precipitations[3],
+        dates[4], precipitations[4]
+    )
+#######################################################################################################
+def extract_prec_year(estacion):
+    query = query_prec_year(estacion)
+    results = data_fetch(query)
+    
+    # Ordenar los resultados en orden ascendente
+    results_asc = sorted(results, key=lambda x: x[1])
 
+    # Obtener los valores máximos y mínimos
+    results_max = results_asc[-5:][::-1]  # Los 5 valores máximos en orden descendente
+    results_min = results_asc[:5]  # Los 5 valores mínimos en orden ascendente
+
+    # Crear las listas de fechas y valores para máximos y mínimos
+    dates_max = ['{}'.format(result[0]) for result in results_max]
+    values_max = ['{:.1f} mm'.format(result[1]) for result in results_max]
+    dates_min = ['{}'.format(result[0]) for result in results_min]
+    values_min = ['{:.1f} mm'.format(result[1]) for result in results_min]
+    # Retornar los valores como una tupla
+    return (
+        dates_max[0], values_max[0],
+        dates_max[1], values_max[1],
+        dates_max[2], values_max[2],
+        dates_max[3], values_max[3],
+        dates_max[4], values_max[4],
+        dates_min[0], values_min[0],
+        dates_min[1], values_min[1],
+        dates_min[2], values_min[2],
+        dates_min[3], values_min[3],
+        dates_min[4], values_min[4]
+    )
+
+
+
+#######################################################################################################
+## G R Á F I C O   P R E C I P I T A C I Ó N 
+#######################################################################################################
+def get_prec(estacion, year):
+
+    df_aux = pd.DataFrame(data_fetch(query_year_prec(estacion, year)), columns=['month', 'day','prec'])
+    df_prec = pd.pivot_table(df_aux, values='prec', index='month', columns='day')
+    #print(df_prec)
+    heat_prec = np.empty((12,31))*np.nan
+    
+    heat_prec = df_prec.to_numpy()
+    return heat_prec
+
+def fig_prec(estacion, year):
+    heat_prec = get_prec(estacion, year)
+
+    customdata=[]
+    customdata.append(heat_prec)
+
+    fig = px.imshow(heat_prec, color_continuous_scale='BuPu',
+                    zmin=0, zmax=20, text_auto='.1f', aspect='auto')
+    values_df = pd.DataFrame(heat_prec).apply(lambda x: np.round(x,1), axis=1).fillna('x').astype('str')
+    fig.update_traces(text=values_df,
+                        texttemplate='%{text}')
+    fig.update_traces(hoverongaps=False, textfont=dict(size=11))
+    fig.update_yaxes(range=[0, 11])
+    fig.update(data=[{'customdata': np.dstack((customdata[0])),
+                    'hovertemplate':'%{x} de %{y}<br>Precipitacion: %{z:.2f}mm.'}])
+
+    fig.update_layout(
+        margin={'b': 10, 'l': 0, 'r': 40, 't': 40},
+        paper_bgcolor='#32383e',
+        plot_bgcolor= '#32383e',
+        yaxis=dict(
+            tickmode='array',
+            ticklen=0,
+            tickvals=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+            ticktext=['Enero ', 'Febrero ', 'Marzo ', 'Abril ', 'Mayo ', 'Junio ', 'Julio ', 'Agosto ', 'Septiembre ',
+                    'Octubre ', 'Noviembre ', 'Diciembre ']
+        ),
+        xaxis=dict(
+            tickmode='array',
+            ticklen=0,
+            tickvals=[n for n in range(31)],
+            ticktext=[str(n + 1) for n in range(31)],
+            side='top'
+        ),
+        coloraxis=dict(
+            colorbar=dict(
+                orientation='h',
+                x=0.2,
+                y=-0.2,
+                thickness=20,
+                len=0.4,
+                tickfont=dict(color='white', size=11),
+                title=dict(text='Precipitación (mm)', font=dict(color='white', size=11)),
+                # ticklabelposition = 'inside'
+            )
+        )
+    )
+    fig.update_xaxes(tickfont_color='white', tickfont_size=11)
+    fig.update_yaxes(tickfont_color='white', tickfont_size=11)
+    fig.update_xaxes(showgrid=False, zeroline=False)
+    fig.update_yaxes(range=[11.5,-0.5], autorange=False, showgrid=False, zeroline=False)
+    return fig
+#######################################################################################################
+## G R Á F I C O   A N O M A L I A S
+#######################################################################################################
 def get_anom(estacion, year):
     df_med = pd.DataFrame(data_fetch(query_tmed(estacion)), columns=['month','day','tmed'])
     df_med = pd.pivot_table(df_med, values='tmed', index='month', columns='day')
-
-    # print(df_med)
-
-    df_aux = pd.DataFrame(data_fetch(query_year(estacion, year)), columns=['month', 'day', 'tmed', 'tmax', 'tmin'])
+    #print(df_med)
+    df_aux = pd.DataFrame(data_fetch(query_year_anom(estacion, year)), columns=['month', 'day', 'tmed', 'tmax', 'tmin'])
     df_heat = pd.pivot_table(df_aux, values='tmed', index='month', columns='day')
     df_max = pd.pivot_table(df_aux, values='tmax', index='month', columns='day')
     df_min = pd.pivot_table(df_aux, values='tmin', index='month', columns='day')
@@ -51,9 +164,9 @@ def get_anom(estacion, year):
     heat_med = heat_anom + df_med.to_numpy()
     heat_max[:shape_mat[0], :shape_mat[1]] = df_max.to_numpy()
     heat_min[:shape_mat[0], :shape_mat[1]] = df_min.to_numpy()
-
     return heat_anom, heat_med, heat_max, heat_min
 
+    
 def fig_anom(estacion, year):
     heat_anom, heat_med, heat_max, heat_min = get_anom(estacion, year)
 
@@ -107,4 +220,86 @@ def fig_anom(estacion, year):
     fig.update_yaxes(tickfont_color='white', tickfont_size=11)
     fig.update_xaxes(showgrid=False, zeroline=False)
     fig.update_yaxes(range=[11.5,-0.5], autorange=False, showgrid=False, zeroline=False)
+    return fig
+#########################################################################################################################
+def get_top_anom(estacion, year):
+    df_med = pd.DataFrame(data_fetch(query_tmed(estacion)), columns=['month','day','tmed'])
+    df_med = pd.pivot_table(df_med, values='tmed', index='month', columns='day')
+
+    df_aux = pd.DataFrame(data_fetch(query_year_anom(estacion, year)), columns=['month', 'day', 'tmed', 'tmax', 'tmin'])
+    df_heat = pd.pivot_table(df_aux, values='tmed', index='month', columns='day')
+
+    heat_anom = np.empty((12,31))*np.nan
+    
+    shape_mat = df_heat.to_numpy().shape
+    months = (df_heat.index.astype(int).to_numpy()-1).tolist()
+
+    if len(months) == 1:
+        heat_anom[:shape_mat[0],:shape_mat[1]] = df_heat.to_numpy()-df_med.to_numpy()[months,range(len(df_heat.to_numpy()))]
+    else:
+        heat_anom[:shape_mat[0],:shape_mat[1]] = df_heat.to_numpy()-df_med.to_numpy()[months,:]
+
+    return heat_anom
+
+def get_anom_month(estacion, year):
+    heat_anom = get_top_anom(estacion, year)
+    
+    anom_month_avg = [(i, np.nanmean(heat_anom[i])) for i in range(len(heat_anom))]
+    anom_month_avg = sorted(anom_month_avg, key=lambda x: x[1], reverse=True)
+    anom_month_avg_2 = sorted(anom_month_avg, key=lambda x: x[1], reverse=False)
+    top5_months = anom_month_avg[:5]
+    down5_months = anom_month_avg_2[:5]
+    results = [(month, year, anomaly) for (month, anomaly) in top5_months]
+    results2 = [(month, year, anomaly) for (month, anomaly) in down5_months]
+    print('TOP5 anomalies', results)
+    dates = ['{} {}'.format(month_list[result[0]], result[1]) for result in results]
+    dates2 = ['{} {}'.format(month_list[result[0]], result[1]) for result in results2]
+
+    anomalies = ['{:.1f} {}'.format(result[2], degree_sign + 'C') for result in results]
+    anomalies2 = ['{:.1f} {}'.format(result[2], degree_sign + 'C') for result in results2]
+    #print('anomalies',anomalies,anomalies2)
+    return (
+        dates[0], anomalies[0],
+        dates[1], anomalies[1],
+        dates[2], anomalies[2],
+        dates[3], anomalies[3],
+        dates[4], anomalies[4],
+        dates2[0], anomalies2[0],
+        dates2[1], anomalies2[1],
+        dates2[2], anomalies2[2],
+        dates2[3], anomalies2[3],
+        dates2[4], anomalies2[4],
+        
+    )
+#######################################################################################################
+## G R Á F I C O   B A R R A S
+#######################################################################################################
+def generate_bar_chart(year, estacion):
+    query = query_prec_month(estacion)
+    results = data_fetch(query)
+    df_filtered = sorted(results, key=lambda x: (x[0], x[1]))
+
+    df = pd.DataFrame(df_filtered, columns=['month', 'year', 'prec'])
+    df_filtered = df[df['year'] == year].astype(int)
+
+    month_names = [month_list[int(result[1]['month']) - 1] + ' ' + str(result[1]['year']) for result in df_filtered.iterrows()]
+
+    # Crear la figura de barras
+    fig = go.Figure()
+    fig.add_trace(go.Bar(x=month_names, y=df_filtered['prec']))
+    fig.update_layout(
+        title='Precipitación para el año {} (Total: {} mm)'.format(year, df_filtered['prec'].sum().round(1)),
+        paper_bgcolor='#32383e',
+        plot_bgcolor= '#32383e',
+        xaxis_title='Mes',
+        yaxis_title='Lluvia mm',
+        font=dict(size=12,color= '#999'),
+    ),
+    fig.update_xaxes(tickfont_color='white', tickfont_size=11)
+    fig.update_yaxes(tickfont_color='white',tickfont_size=11)
+    fig.update_xaxes(showgrid=False, zeroline=False)
+    fig.update_yaxes(showgrid=True,gridcolor='LightBlue',gridwidth=1,)
+    #fig.update_traces(texttemplate='%{text:.2s}', textposition='outside')
+    fig.update_traces(marker_color='rgb(158,202,225)', marker_line_color='rgb(8,48,107)',
+                  marker_line_width=1.5, opacity=1)
     return fig
--- layouts/callbacks.py
+++ layouts/callbacks.py
@@ -1,8 +1,10 @@
 import pandas as pd
 from dash import Input, Output
-from layouts.aux_functions import extract_data, fig_anom
+from layouts.aux_functions import extract_data, extract_prec_month,extract_prec_year,fig_anom,fig_prec,get_anom_month,generate_bar_chart
 from data.postgresql.data_fetch import data_fetch
 from data.postgresql.queries import query_estaciones, query_from
+import dash
+import plotly.graph_objs as go
 
 df_estaciones = pd.DataFrame(data_fetch(query_estaciones()), columns=['latitud', 'provincia', 'altitud', 'indicativo', 'nombre', 'indsinop', 'longitud'])
 
@@ -21,14 +23,20 @@ def register_callbacks(app):
 
     @app.callback(Output('desde_max', 'children'),
                 Output('desde_min', 'children'),
+                Output('desde_med', 'children'),
                 Output('desde_amp', 'children'),
                 Output('desde_prec', 'children'),
+                Output('desde_prec_month', 'children'),
+                Output('desde_prec_year', 'children'),
+                Output('desde_anom_month', 'children'),
                 Output('year_input', 'min'),
                 Input('nom_est', 'children'))
     def update_year(nombre):
         year_max = data_fetch(query_from(nombre, 'tmax'))[0][0].year
         year_prec = data_fetch(query_from(nombre, 'prec'))[0][0].year
-        return (str(year_max), str(year_max), str(year_max), str(year_prec), year_max)
+        print('YEARMAX',year_max)
+        print('YEARPREC',year_prec)
+        return (str(year_max), str(year_max), str(year_max), str(year_max),str(year_max),str(year_max),str(year_max),str(year_max), year_max)
 
     @app.callback(
         Output('date_max_1', 'children'),
@@ -65,6 +73,23 @@ def register_callbacks(app):
         return extract_data('tmin', nombre, tab, month)
 
     @app.callback(
+        Output('date_med_1', 'children'),
+        Output('med_1', 'children'),
+        Output('date_med_2', 'children'),
+        Output('med_2', 'children'),
+        Output('date_med_3', 'children'),
+        Output('med_3', 'children'),
+        Output('date_med_4', 'children'),
+        Output('med_4', 'children'),
+        Output('date_med_5', 'children'),
+        Output('med_5', 'children'),
+        Input('nom_est', 'children'),
+        Input('tabs_med', 'active_tab'),
+        Input('month_radio', 'value'))
+    def extract_tmed(nombre, tab, month):
+        return extract_data('tmed', nombre, tab, month)
+
+    @app.callback(
         Output('date_amp_1', 'children'),
         Output('amp_1', 'children'),
         Output('date_amp_2', 'children'),
@@ -80,7 +105,9 @@ def register_callbacks(app):
         Input('month_radio', 'value'))
     def extract_amp(nombre, tab, month):
         return extract_data('tmax-tmin', nombre, tab, month)
-
+    #-------------------------------------------------------
+    # PRECIPITACIÓN DIARIA
+    #-------------------------------------------------------
     @app.callback(
         Output('date_prec_1', 'children'),
         Output('prec_1', 'children'),
@@ -96,11 +123,175 @@ def register_callbacks(app):
         Input('month_radio', 'value'))
     def extract_prec(nombre, month):
         return extract_data('prec', nombre, 'max', month)
+    
+    #-------------------------------------------------------
+    # PRECIPITACIÓN MENSUAL
+    #-------------------------------------------------------
+    @app.callback(
+        Output('date_preci_month_1', 'children'),
+        Output('preci_month_1', 'children'),
+        Output('date_preci_month_2', 'children'),
+        Output('preci_month_2', 'children'),
+        Output('date_preci_month_3', 'children'),
+        Output('preci_month_3', 'children'),
+        Output('date_preci_month_4', 'children'),
+        Output('preci_month_4', 'children'),
+        Output('date_preci_month_5', 'children'),
+        Output('preci_month_5', 'children'),
+        Input('nom_est', 'children')
+    )
+    def extract_prec(nombre):
+        result = extract_prec_month(nombre)
+        return (
+            result[0], result[1],
+            result[2], result[3],
+            result[4], result[5],
+            result[6], result[7],
+            result[8], result[9]
+        )
+    #-------------------------------------------------------
+    # PRECIPITACIÓN ANUAL
+    #-------------------------------------------------------
+    @app.callback(
+        Output('date_preci_year_1', 'children'),
+        Output('preci_year_1', 'children'),
+        Output('date_preci_year_2', 'children'),
+        Output('preci_year_2', 'children'),
+        Output('date_preci_year_3', 'children'),
+        Output('preci_year_3', 'children'),
+        Output('date_preci_year_4', 'children'),
+        Output('preci_year_4', 'children'),
+        Output('date_preci_year_5', 'children'),
+        Output('preci_year_5', 'children'),
+        Input('nom_est', 'children'),
+        Input('tabs_prec_year', 'active_tab')
+    )
+    def extract_prec(nombre,current_tab):
+        results = extract_prec_year(nombre)
+        
+        dates_max = results[0:10:2]  # Fechas de los máximos valores de precipitación
+        values_max = results[1:10:2]  # Valores de los máximos valores de precipitación
+        dates_min = results[10:20:2]  # Fechas de los mínimos valores de precipitación
+        values_min = results[11:20:2]  # Valores de los mínimos valores de precipitación
+        if current_tab == 'tab_preci_year_max':
+            return (
+                dates_max[0], values_max[0],
+                dates_max[1], values_max[1],
+                dates_max[2], values_max[2],
+                dates_max[3], values_max[3],
+                dates_max[4], values_max[4]
+            )
+        elif current_tab == 'tab_preci_year_min':
+            return (
+                dates_min[0], values_min[0],
+                dates_min[1], values_min[1],
+                dates_min[2], values_min[2],
+                dates_min[3], values_min[3],
+                dates_min[4], values_min[4]
+            )
 
+
+    #-------------------------------------------------------
+    # TOP5 ANOMALIAS MENSUALES
+    #-------------------------------------------------------
+    @app.callback(
+        Output('date_month_anom_1', 'children'),
+        Output('month_anom_1', 'children'),
+        Output('date_month_anom_2', 'children'),
+        Output('month_anom_2', 'children'),
+        Output('date_month_anom_3', 'children'),
+        Output('month_anom_3', 'children'),
+        Output('date_month_anom_4', 'children'),
+        Output('month_anom_4', 'children'),
+        Output('date_month_anom_5', 'children'),
+        Output('month_anom_5', 'children'),
+        Input('nom_est', 'children'),
+        Input('year_input_anom', 'value'),
+        Input('tabs_anom', 'active_tab')
+    )
+    def extract_top5_anom(nombre, year, current_tab):
+        result = get_anom_month(nombre, year)
+        top_dates = result[:10][::2]  # Fechas de las anomalías máximas
+        top_anomalies = result[:10][1::2]  # Valores de las anomalías máximas
+        down_dates = result[10:][::2]  # Fechas de las anomalías mínimas
+        down_anomalies = result[10:][1::2]  # Valores de las anomalías mínimas    
+
+        if current_tab == 'tabs_anom_max':
+            return (
+                top_dates[0], top_anomalies[0],
+                top_dates[1], top_anomalies[1],
+                top_dates[2], top_anomalies[2],
+                top_dates[3], top_anomalies[3],
+                top_dates[4], top_anomalies[4]
+            )
+        elif current_tab == 'tabs_anom_min':
+            return (
+                down_dates[0], down_anomalies[0],
+                down_dates[1], down_anomalies[1],
+                down_dates[2], down_anomalies[2],
+                down_dates[3], down_anomalies[3],
+                down_dates[4], down_anomalies[4]
+            )
+
+
+
+    #-------------------------------------------------------
+    # HEATMAP PRECIPITACIÓN 
+    #-------------------------------------------------------
+    @app.callback(
+        Output('graph_prec', 'figure'),
+        Output('year_input_prec', 'max'),
+        Input('nom_est', 'children'),
+        Input('year_input_prec', 'value'),
+        Input('graph_select', 'value')
+    )
+    def plot_prec(nombre, year, graph_select):
+        if graph_select == 'prec':  
+            return fig_prec(nombre, year), pd.Timestamp.today().year
+        else:
+
+            pass
+
+    #-------------------------------------------------------
+    # HEATMAP ANOMALÍA
+    #-------------------------------------------------------
     @app.callback(
         Output('graph_anom', 'figure'),
-        Output('year_input','max'),
+        Output('year_input_anom', 'max'),
         Input('nom_est', 'children'),
-        Input('year_input', 'value'))
-    def plot_anom(nombre, year):
-        return (fig_anom(nombre, year), pd.Timestamp.today().year)
+        Input('year_input_anom', 'value'),
+        Input('graph_select', 'value') 
+    )
+    def plot_anom(nombre, year, graph_select):
+        if graph_select == 'anom': 
+            return fig_anom(nombre, year), pd.Timestamp.today().year
+        else:
+
+            pass
+        
+    @app.callback(
+    Output('card_graph_prec', 'style'),
+    Output('card_graph_anom', 'style'),
+    Input('graph_select', 'value')
+    )
+    def update_graph_select(value):
+        if value == 'prec':
+            return {'display': 'block'}, {'display': 'none'}
+        elif value == 'anom':
+            return {'display': 'none'}, {'display': 'block'}
+        else:
+            return dash.no_update, dash.no_update
+    #-------------------------------------------------------
+    # GRAFICO DE BARRAS
+    #-------------------------------------------------------
+    @app.callback(
+        Output('bar_chart', 'figure'),
+        Input('year_input_prec', 'value'), 
+        Input('nom_est', 'children')
+    )
+    def update_bar_chart(year, estacion):
+        if year is None:
+           
+            return go.Figure()
+
+        return generate_bar_chart(year, estacion)
